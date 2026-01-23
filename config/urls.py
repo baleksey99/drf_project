b973f0027737db1e8a66186d7fbc8f54e65453fb
Алексей <abe26@yandex.ru>
2026-01-23 11:04:11 +0300

    payment

--- config/urls.py
+++ config/urls.py
@@ -1,12 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.http import HttpResponse
-
-def api_root(request):
-    return HttpResponse("Добро пожаловать в API LMS!", status=200)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', api_root),  # Обработчик для /api/
-    path('api/', include('materials.urls')),  # Вложенные URL
+    path('api/', include('materials.urls')),      # Маршруты для курсов/уроков
+    path('api/', include('users.urls')),          # Маршруты для платежей/пользователей
 ]
