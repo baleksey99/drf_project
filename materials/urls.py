9f69e925762f60f8ca00c753d49d07d197c1f20f
Алексей <abe26@yandex.ru>
2026-01-26 17:48:45 +0300

    внесение изменений

--- materials/urls.py
+++ materials/urls.py
@@ -1,8 +1,15 @@
 
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework.routers import DefaultRouter
+from .views import CourseViewSet
+
+
+router = DefaultRouter()
+router.register(r'courses', CourseViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('courses/', views.CourseList.as_view(), name='course-list'),
     path('courses/<int:pk>/', views.CourseDetail.as_view(), name='course-detail'),
     path('lessons/', views.LessonList.as_view(), name='lesson-list'),
