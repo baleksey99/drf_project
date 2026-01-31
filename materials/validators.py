3f248b11a41591696d4032a4ec8aa40b970593ee
Алексей <abe26@yandex.ru>
2026-01-31 22:19:41 +0300

    домашка.4

--- materials/validators.py
+++ materials/validators.py
@@ -3,7 +3,19 @@ from rest_framework import serializers
 from django.core.exceptions import ValidationError
 
 def validate_youtube_link(value):
-    youtube_regex = r'(https?://)?(www\.)?(youtube\.com|youtu\.be)/.+'
+    """
+    Проверяет, что URL ведёт на YouTube (youtube.com или youtu.be).
+    Поддерживает:
+    - https://www.youtube.com/watch?v=...
+    - https://youtu.be/...
+    - embed-ссылки
+    - плейлисты
+    """
+    youtube_regex = (
+        r'^(https?://)?(www\.)?'
+        r'(youtube\.com|youtu\.be|youtube-nocookie\.com)/'
+        r'(watch\?v=|embed/|v/|shorts/|playlist\?list=|)[^\s]+$'
+    )
     if not re.match(youtube_regex, value):
         raise ValidationError('Разрешены только ссылки на YouTube (youtube.com, youtu.be).')
     return value
@@ -15,6 +27,7 @@ class YouTubeLinkValidator:
     def __call__(self, value):
         if not value:
             return
-        youtube_regex = r'(https?://)?(www\.)?(youtube\.com|youtu\.be)/.+'
-        if not re.match(youtube_regex, value):
-            raise serializers.ValidationError("Некорректная ссылка на YouTube")
+        try:
+            validate_youtube_link(value)
+        except ValidationError as e:
+            raise serializers.ValidationError(e.message)
