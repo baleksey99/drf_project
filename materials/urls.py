95a584562c0f0dace15828fa6cbc962d121fbf7a
Алексей <abe26@yandex.ru>
2026-01-31 22:19:41 +0300

    домашка.4

--- materials/urls.py
+++ materials/urls.py
@@ -1,25 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CourseViewSet, LessonViewSet, subscribe, unsubscribe
-
+from .views import CourseViewSet, LessonViewSet
 
 router = DefaultRouter()
 router.register(r'courses', CourseViewSet, basename='course')
 router.register(r'lessons', LessonViewSet, basename='lesson')
 
-
 urlpatterns = [
-    path('api/', include(router.urls)),  # ← Добавлен префикс /api/
-
-
-    path(
-        'api/courses/<int:course_id>/subscribe/',
-        subscribe,
-        name='subscribe'
-    ),
-    path(
-        'api/courses/<int:course_id>/unsubscribe/',
-        unsubscribe,
-        name='unsubscribe'
-    ),
+    path('api/', include(router.urls)),
 ]
